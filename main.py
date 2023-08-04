1290975975c58a8c1145230d1534050732f803fa
Santiago Serpa <ss5gc4@gmail.com>
2023-08-04 13:50:58 -0500

    V 1.0.1 api gateway sistema de votaciones

---
+++ main.py
@@ -0,0 +1,123 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+import json
+from waitress import serve
+import datetime
+import requests
+import re
+
+app=Flask(__name__)
+cors = CORS(app)
+from flask_jwt_extended import create_access_token, verify_jwt_in_request
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+app.config["JWT_SECRET_KEY"]="super-secret" #Cambiar por el que se conveniente
+jwt = JWTManager(app)
+@app.route("/login", methods=["POST"])
+def create_token():
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url=dataConfig["url-backend-security"]+'/usuarios/validar'
+    response = requests.post(url, json=data, headers=headers)
+    if response.status_code == 202:
+        user = response.json()
+        expires = datetime.timedelta(seconds=60 * 60*24)
+        access_token = create_access_token(identity=user, expires_delta=expires)
+        return jsonify({"token": access_token, "user_id": user})
+    else:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+@app.before_request
+def before_request_callback():
+    endPoint = limpiarURL(request.path)
+    excludedRoutes = ["/login"]
+    if request.path in excludedRoutes:
+        pass
+    elif verify_jwt_in_request():
+        usuario = get_jwt_identity()
+        rol_del_usuario = usuario['Acceso autorizado'].get('rol')
+        if rol_del_usuario is not None:
+            tienePermiso = validarPermiso(endPoint, request.method, rol_del_usuario["_id"])
+            if not tienePermiso:
+                return jsonify({"message": "Permission denied 1"}), 401
+        else:
+            return jsonify({"message": "Permission denied 2"}), 401
+
+def limpiarURL(url):
+    partes = url.split("/")
+    for laParte in partes:
+        if re.search('\\d', laParte):
+            url = url.replace(laParte, "?")
+    return url
+def validarPermiso(endPoint,metodo,idRol):
+    url=dataConfig["url-backend-security"]+"/permisos-roles/validar-permiso/rol/"+str(idRol)
+    tienePermiso=False
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    body={
+        "url":endPoint,
+        "metodo":metodo
+    }
+    response = requests.get(url,json=body, headers=headers)
+    try:
+        data=response.json()
+        if("_id" in data):
+            tienePermiso=True
+    except:
+        pass
+    return tienePermiso
+###################################################################################
+@app.route("/candidatos",methods=['GET'])
+def getCandidatos():
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-votaciones"] + '/api/v1/candidatos'
+    response = requests.get(url, headers=headers)
+    json = response.json()
+    return jsonify(json)
+@app.route("/candidatos",methods=['POST'])
+def crearCandidato():
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-votaciones"] + '/api/v1/candidatos'
+    response = requests.post(url, headers=headers,json=data)
+    json = response.json()
+    return jsonify(json)
+@app.route("/candidatos/<string:id>",methods=['GET'])
+def getCandidato(id):
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-votaciones"] + '/api/v1/candidatos/'+id
+    response = requests.get(url, headers=headers)
+    json = response.json()
+    return jsonify(json)
+@app.route("/candidatos/<string:id>",methods=['PUT'])
+def modificarCandidato(id):
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-votaciones"] + '/api/v1/candidatos/'+id
+    print(url)
+    response = requests.put(url, headers=headers, json=data)
+    json = response.json()
+    return jsonify(json)
+@app.route("/candidatos/<string:id>",methods=['DELETE'])
+def eliminarCandidato(id):
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-votaciones"] + '/api/v1/candidatos/' + id
+    response = requests.delete(url, headers=headers)
+    json = response.json()
+    return jsonify(json)
+@app.route("/",methods=['GET'])
+def test():
+    json = {}
+    json["message"]="Server running ..."
+    return jsonify(json)
+
+def loadFileConfig():
+    with open('config.json') as f:
+        data = json.load(f)
+    return data
+if __name__=='__main__':
+    dataConfig = loadFileConfig()
+    print("Server running : "+"http://"+dataConfig["url-backend"]+":" + str(dataConfig["port"]))
+    serve(app,host=dataConfig["url-backend"],port=dataConfig["port"])
